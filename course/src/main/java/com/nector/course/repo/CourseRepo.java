316f1e1ce35318a963f1245902d5bc5a28a1cfcc
jagadeeshwaran001 <jagadeeshwaran2601@gmail.com>
2025-08-04 15:57:57 +0530

    changes delted

--- course/src/main/java/com/nector/course/repo/CourseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.nector.course.repo;
-
-import com.nector.course.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepo extends JpaRepository<Course, Long> {
-}
--- student/src/main/java/com/nector/student/exception/ErrorGettingResponseThroughHttpRequest.java
+++
@@ -1,7 +0,0 @@
-package com.nector.student.exception;
-
-public class ErrorGettingResponseThroughHttpRequest extends RuntimeException{
-    public ErrorGettingResponseThroughHttpRequest(String message) {
-        super(message);
-    }
-}
